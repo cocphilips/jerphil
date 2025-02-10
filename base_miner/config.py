47d1abd507ecb2f617a95442ce88bc41d411cd22
cocphilips <57974352+cocphilips@users.noreply.github.com>
2025-02-10 20:42:26 +0700

    Update config.py

--- base_miner/config.py
+++ base_miner/config.py
@@ -32,8 +32,8 @@ VIDEO_DATASETS = {
 FACE_IMAGE_DATASETS = {
     "real": [
         {"path": "bitmind/ffhq-256_training_faces", "name": "base_transforms"},
-        {"path": "bitmind/celeb-a-hq_training_faces", "name": "base_transforms"}
-
+        {"path": "bitmind/celeb-a-hq_training_faces", "name": "base_transforms"},
+        {"path": "goodfellowliu/CelebA", "name": "base_transforms"}
     ],
     "fake": [
         {"path": "bitmind/ffhq-256___stable-diffusion-xl-base-1.0_training_faces", "name": "base_transforms"},
