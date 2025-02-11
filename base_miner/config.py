4470fc09f933abf94841aae258a9d6657d6257a0
cocphilips <57974352+cocphilips@users.noreply.github.com>
2025-02-11 19:42:42 +0700

    config.py

--- base_miner/config.py
+++ base_miner/config.py
@@ -11,12 +11,18 @@ IMAGE_DATASETS = {
         {"path": "bitmind/celeb-a-hq"},
         {"path": "bitmind/ffhq-256"},
         {"path": "bitmind/MS-COCO-unique-256"},
-        {"path": "evanarlian/imagenet_1k_resized_256"}
+        {"path": "evanarlian/imagenet_1k_resized_256"},
+        {"path": "philipsnr/aslitrain"},
+        {"path": "philipsnr/aslitrain2"},
+        {"path": "philipsnr/aslitrain3"},
+        {"path": "philipsnr/aslitrain4"},
+        {"path": "philipsnr/aslitrain5"}
     ],
     "fake": [
         {"path": "bitmind/bm-realvisxl"},
         {"path": "bitmind/bm-mobius"},
-        {"path": "bitmind/bm-sdxl"}
+        {"path": "bitmind/bm-sdxl"},
+        {"path": "philipsnr/palsutrain"}
     ]
 }
 
@@ -39,6 +45,7 @@ FACE_IMAGE_DATASETS = {
     ],
     "fake": [
         {"path": "bitmind/ffhq-256___stable-diffusion-xl-base-1.0_training_faces", "name": "base_transforms"},
-        {"path": "bitmind/celeb-a-hq___stable-diffusion-xl-base-1.0___256_training_faces", "name": "base_transforms"}
+        {"path": "bitmind/celeb-a-hq___stable-diffusion-xl-base-1.0___256_training_faces", "name": "base_transforms"},
+        {"path": "philipsnr/muka_palsu", "name": "base_transforms"}
     ]
 }
